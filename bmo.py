c46b42e6a039c6f3db37715a9fb8fc9ce6426def
jinyeom <jinyeom95@gmail.com>
2018-08-07 23:32:09 -0500

    some progress

--- bmo.py
+++ bmo.py
@@ -8,14 +8,6 @@ import fire
 from slackclient import SlackClient
 
 
-class BmoCommand(Enum):
-    Hello = "hello"
-
-class BmoResponse(Enum):
-    Default = "Not sure what you mean..."
-    Hello = "Hello!"
-    
-
 class Bmo(object):
     def __init__(self, token_env_var: str = "SLACK_BOT_TOKEN"):
         self._logger = logging.getLogger(self.__class__.__name__)
@@ -28,15 +20,14 @@ class Bmo(object):
         self._slack_client = SlackClient(token)
         self._user_id = self._slack_client.api_call("auth.test")["user_id"]
 
-
-
-        
-
     def _get_oauth_access_token(self, token_env_var: str):
         token = os.environ.get(token_env_var)
         if token is None:
             raise ValueError(f"No environment variable named {token_env_var}")
         return token
+
+    def listen(slack_events):
+        
 
     def run(self):
         pass
